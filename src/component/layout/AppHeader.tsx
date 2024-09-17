9acc3df89a18261eafb5a74a87d7efbf0839ee66
e-mario.coppola <e-mario,coppola@wolterskluver.com>
2024-09-18 00:55:27 +0200

    minor header fix

--- src/component/layout/AppHeader.tsx
+++ src/component/layout/AppHeader.tsx
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export const AppHeader = () => {
- return <Flex pt="8" justify="space-between" align="center" minH="110px">
+ return <Flex justify="space-between" align="center" minH="5rem" padding="0 3rem" position="sticky">
  <Link to="/">
    HOMEPAGE
  </Link>
